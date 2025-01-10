1e775d4394bedfc36f3b532a02ed174c5492b7fd
jikkskv <jikkskv@gmail.com>
2025-01-10 20:12:46 +0530

    [atm-management-service]Initial commit for providing functions for atm management service

---
+++ atm-management-cli/src/test/java/com/xyzbank/atm/atm_management_service/AtmManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyzbank.atm.atm_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
